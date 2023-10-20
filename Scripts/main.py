53dbfc48351abc9037cbbb81065d77000d39fbe1
ryuaus26 <yunryu1217@gmail.com>
2023-10-20 14:51:25 -0700

    Update

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,10 +1,10 @@
 import keras
 import os
+import tensorflow as tf
 
 
+train_path = os.path.join("Data","asl_alphabet_train/asl_alphabet_train")
 
-train_path = os.path.join("Data","asl_alphabet_test/asl_alphabet_train")
-test_path = os.path.join("Data","asl_alphabet_test/asl_alphabet_test")
 
 #define tuning model
 
@@ -13,6 +13,7 @@ test_path = os.path.join("Data","asl_alphabet_test/asl_alphabet_test")
 EPOCHS = 100
 IMG_SIZE = (200,200)
 LR = 0.01
+BATCH_SIZE = 32
 
 VGG16 = keras.applications.VGG16(include_top=False,input_shape=(IMG_SIZE + (3,)),classes=29)
 
@@ -20,13 +21,22 @@ VGG16 = keras.applications.VGG16(include_top=False,input_shape=(IMG_SIZE + (3,))
 
 
 train_generator  = keras.preprocessing.image.ImageDataGenerator(
-    rescale=1./255
+    rescale=1./255,
+    validation_split=0.2
 )
 
 train_data_gen = train_generator.flow_from_directory(
     directory=train_path,
     target_size=IMG_SIZE,
-    class_mode=""
+    class_mode="categorical",
+    batch_size=BATCH_SIZE,
+    
 )
+
+
+
+#Fine Tune the model
+preprocess_input = tf.keras.applications.VGG16.preprocess_input
+
 
 
